cb3d3e7198222a4e27ff50d2fe22a9a26613cef8
divineamunega <divineamunega@gmail.com>
2024-09-24 19:48:22 +0100

    BASE_

--- src/features/articles/Articles.jsx
+++ src/features/articles/Articles.jsx
@@ -1,4 +1,4 @@
-import { FaComments, FaHeart, FaRegHeart } from "react-icons/fa6";
+import { FaComments, FaRegHeart } from "react-icons/fa6";
 import { useLoaderData } from "react-router-dom";
 
 const Articles = () => {
